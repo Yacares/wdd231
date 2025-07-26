1540949b4e512ac0d6e773bc6b63fe999e29980f
unknown <lautarogrodriguez96@gmail.com>
2025-07-26 17:08:50 -0300

    second commit

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.getElementById('hamburger');
-  const navMenu = document.querySelector('nav.wayfinder ul');
+  const navMenu = document.querySelector('nav.wayfinder'); // << CAMBIADO
 
   hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
+    navMenu.classList.toggle('show'); // << ahora sí aplica al correcto
   });
 });
